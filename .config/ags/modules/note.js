283b89850567ced0c222d918f72debb151f498b4
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-08-15 22:27:08 +0200

    updated note widget, added service and made the tooltip refresh its content periodically.

--- .config/ags/modules/note.js
+++ .config/ags/modules/note.js
@@ -1,5 +1,33 @@
+const { Service, Widget } = ags;
 const { exec, execAsync } = ags.Utils;
-const { Widget } = ags;
+
+let first = false;
+
+class NoteService extends Service {
+    static { Service.register(this); }
+
+    get callNote() {
+        execAsync(['bash', '-c', "~/.config/waybar/scripts/note.sh ags"]).catch(print);
+        // this.emit('changed');
+    }
+
+    constructor() {
+        super();
+        this._note = null;
+    }
+
+    get note() { return this._note; }
+    setNote(text) { this._note = text; }
+}
+
+class Note {
+    static { Service.export(this, 'Note'); }
+    static instance = new NoteService;
+    static get callNote() { return Note.instance.callNote; }
+
+    static get note() { return Note.instance.note; }
+    static setNote(text) { Note.instance.setNote(text); }
+}
 
 Widget.widgets['note'] = props => Widget({
     child: {
@@ -8,11 +36,22 @@ Widget.widgets['note'] = props => Widget({
     },
     ...props,
     type: 'button',
-    tooltip: exec('cat /home/bajic/Documents/note'), //todo handle this one in nixos
+    tooltip: '',
     onClick: () => {
         execAsync(['bash', '-c', "codium ~/Documents/note"]).catch(print);
     },
-    // onSecondaryClick: () => {
-    //     execAsync(['bash', '-c', ""]).catch(print);
-    // },
+    connections: [[10000, button => {
+        if (!first) {
+            first = true;
+        } else {
+            Note.callNote
+        }
+
+        let tooltip = Note.note;
+
+        if (tooltip) {
+            button.set_tooltip_text(tooltip);
+        }
+    }]],
+    onSecondaryClick: Note.callNote
 });
