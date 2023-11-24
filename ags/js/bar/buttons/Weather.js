acb7fd0024a5cc702112ec8a131c3749938f197a
Test Test <jlksdjlfsdjklfsdjf4@protonmail.com>
2023-11-24 23:13:11 +0100

    conflicts, setCss

--- ags/js/bar/buttons/Weather.js
+++ ags/js/bar/buttons/Weather.js
@@ -42,49 +42,49 @@ export const Temperature = props => Widget.Label({
 function weatherBackgroundStyle(icon, box) {
     switch (icon) {
         case "🌇": {//sunset
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #ff6f61, #ffca58, #f0e68c);
                 color: #000000;
             `);
             break;
         }
         case "🌄": {//sunrise   
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #ffcc00, #ff6f61, #ff5e62, #d55f74);
                 color: #000000;
             `);
             break;
         }
         case "🌤": {//few clouds
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #80b3ff, #ffffff, #ffdb4d);
                 color: #000000;
             `);
             break;
         }
         case "🌩": {//thunderstorm
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #0c0e23, #1a1c38, #121320, #0c0e23);
                 color: #ffffff;
             `);
             break;
         }
         case "🌑": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #2c3e50, #1a2533);
                 color: #ffffff;
             `);
             break;
         }
         case "🌕": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #001f3f, #002f4f, #003f5f, #004f6f, #005f7f);
                 color: #ffffff;
             `);
             break;
         }
         case "☀️": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #ffeb99, #ffe580, #ffd866, #ffcf4c, #ffc333);
                 color: #000000;
             `);
@@ -92,119 +92,119 @@ function weatherBackgroundStyle(icon, box) {
         }
         case "☁":
         case "☁️": {//cloudy
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #c4c4c4, #d1d1d1, #dedede, #ebebeb, #f8f8f8);
                 color: #000000;
             `);
             break;
         }
         case "": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #1c2331, #212a38, #263141, #2b3749, #303d51);
                 color: #ffffff;
             `);
             break;
         }
         case "": {//fog
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #d8d8d8, #e2e2e2, #ececec, #f6f6f6, #ffffff);
                 color: #000000;
             `);
             break;
         }
         case "": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #1c2331, #1c2331, #1c2331, #293547, #38475f);
                 color: #ffffff;
             `);
             break;
         }
         case "⛈️": {//heavy rain
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #2c3e50, #34495e, #2c3e50, #34495e, #2c3e50);
                 color: #000000;
             `);
             break;
         }
         case "󰙾": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #050818, #070b1d, #0a0e22, #0d1126, #10152b);
                 color: #ffffff;
             `);
             break;
         }
         case "🌦️": {//light rain
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #547aad, #6692b8, #78a9c3, #8abfd0, #9cd7dd);
                 color: #000000;
             `);
             break;
         }
         case "": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #0e1620, #121c2a, #162133, #18273c, #1c2c46);
                 color: #ffffff;
             `);
             break;
         }
         case "⛅": {//partly cloudy
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #a8c9f0, #c4dfea, #f0f0cc, #ffd700, #f0f0cc, #c4dfea, #a8c9f0);
                 color: #000000;
             `);
             break;
         }
         case "": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #040d1c, #081427, #0c1a32, #101f3d, #142348);
                 color: #ffffff;
             `);
             break;
         }
         case "🌧️": {//rain showers
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #5e7d99, #6a8ba6, #7698b3, #87a5bf, #97b3cb, #a6c0d8, #b4cedf);
                 color: #000000;
             `);
             break;
         }
         case "": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #050818, #070b1d, #0a0e22, #0d1126, #10152b);
                 color: #ffffff;
             `);
             break;
         }
         case "🌨": {//snow
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #d0e6ec, #e0f0f5, #f0f5f9, #f5fafd, #ffffff);
                 color: #000000;
             `);
             break;
         }
         case "": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #0e1620, #162133, #1c263f, #232c4c, #293259);
                 color: #ffffff;
             `);
             break;
         }
         case "🌨️": {//sleet
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #d0e6ec, #e0f0f5, #f0f5f9, #ffffff, #f0f5f9, #e0f0f5, #d0e6ec);
                 color: #000000;
             `);
             break;
         }
         case "": {
-            box.setStyle(`
+            box.setCss(`
                 background: linear-gradient(to bottom, #050818, #08142a, #0c1d3b, #101f47, #142556);
                 color: #ffffff;
             `);
             break;
         }
         default: {
-            box.setStyle(`
+            box.setCss(`
                 background: red;
                 color: blue;
             `);
